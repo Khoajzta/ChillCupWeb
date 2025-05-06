6f0bcbed6b4a23b47302b2aa7fbd745e0f8cf097
Khoajzta <101576530+Khoajzta@users.noreply.github.com>
2025-05-06 14:28:25 +0700

    Kết nối my sql

--- src/server.js
+++ src/server.js
@@ -3,14 +3,29 @@ require("dotenv").config();
 const express = require("express");
 const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
-const app = express();
 
+const mysql = require("mysql2");
+
+const app = express();
 const port = process.env.PORT;
 // Cấu hình view folder và view engine
 configViewEngine(app);
 
 //khai báo route
 app.use("/", webRoutes);
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3307,
+  user: "root",
+  database: "ChillCup",
+  password: "123456",
+});
+
+connection.query("select * from SanPham sp", function (err, results, fields) {
+  console.log(">>>results", results);
+  console.log(">>>fields = ", fields);
+});
 
 // Server listen
 app.listen(port, () => {
