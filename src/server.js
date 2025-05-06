785a4ea5ba1ed8a6f1849c35a6451896b0d580d7
Khoajzta <101576530+Khoajzta@users.noreply.github.com>
2025-05-06 14:42:55 +0700

    tách file kết nối database

--- src/server.js
+++ src/server.js
@@ -3,8 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
-
-const mysql = require("mysql2");
+const connection = require("./config/database");
 
 const app = express();
 const port = process.env.PORT;
@@ -14,17 +13,8 @@ configViewEngine(app);
 //khai báo route
 app.use("/", webRoutes);
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3307,
-  user: "root",
-  database: "ChillCup",
-  password: "123456",
-});
-
 connection.query("select * from SanPham sp", function (err, results, fields) {
   console.log(">>>results", results);
-  console.log(">>>fields = ", fields);
 });
 
 // Server listen
